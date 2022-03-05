11d132957b89f2be2f1c231ef57badeccf7dc90b
C.N <cn.mbhalati@gmail.com>
2022-03-05 15:50:41 +0200

    Complete most of the calc logic

--- index.js
+++ index.js
@@ -14,17 +14,56 @@ const divide = (a,b) => {
     return 'Hey! No division by 0'
 }
 
-const operate = (a,b, operator) => {
+// Operate function
+const operate = (a, b, operator) => {
     if (operator == '+')
         return add(a,b)
     if (operator == '-')
         return subtract(a,b)
-    if (operator == '*')
+    if (operator == '×')
         return multiply(a,b)
-    if (operator == '/')
+    if (operator == '÷')
         return divide(a,b)
 }
 
-const answer = operate(1,0,'/');
+//User inputs defaults
+let a, b, operator, answer
 
-console.log(answer)
+const calcDisplay = document.querySelector('.display p');
+calcDisplay.textContent = '80085' // Default Display Content
+
+const clear = () => {
+    a = undefined;
+    b = undefined;
+    operator = undefined;
+    answer = undefined;
+    calcDisplay.textContent = undefined
+}
+
+const showDisplay = (event) => {
+    calcDisplay.textContent = event.target.textContent
+    if (
+        event.target.textContent == '+' ||
+        event.target.textContent == '-' ||
+        event.target.textContent == '÷' ||
+        event.target.textContent == '×'
+    ) { 
+        operator = event.target.textContent
+        console.log(`Operator: ${operator}`)
+    } else if (event.target.textContent == 'Clear') {
+            clear()
+    } else if (event.target.textContent == '=' ) {
+        answer = operate(a, b, operator)
+        calcDisplay.textContent = answer
+        console.log(`Answer: ${answer}`)
+    } else if (operator) {
+        b = parseInt(event.target.textContent)
+        console.log(`b: ${b}`)
+    } else {
+        a = parseInt(event.target.textContent)
+        console.log(`a: ${a}`)
+    }
+}
+
+const btns = document.querySelectorAll('button');
+btns.forEach(btn => btn.addEventListener('click', showDisplay))
